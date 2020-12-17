6a11d138c99780bfb06d18128810fc3ffc546f01
Raz_Kissos <kissosrazz@gmail.com>
2020-12-17 12:57:05 +0200

    Added gitignore and option do operate on text files

--- CeaserTool.py
+++ CeaserTool.py
@@ -1,4 +1,5 @@
 import argparse
+import os.path as path
 
 
 def read_arguments():
@@ -38,6 +39,13 @@ def read_arguments():
         help="The text to be operated on in calculation.",
         dest="string",
     )
+    parser.add_argument(
+        "--file",
+        default=None,
+        type=str,
+        help="The text file path to be operated on in calculation.",
+        dest="path",
+    )
 
     args = parser.parse_args()
 
@@ -49,6 +57,20 @@ def read_arguments():
         raise ValueError(
             "Neither decrypt nor encrypt have been selected, please select one!"
         )
+    if args.path and args.string:
+        raise ValueError(
+            "Both string and text file path were given, please specify only one!"
+        )
+    if not args.path and not args.string:
+        raise ValueError(
+            "Neither a string nor a text file path were specified, please select one!"
+        )
+    if args.path and not path.exists(args.path):
+        raise FileNotFoundError(
+            f"Could not find specified file to be operated on.\nFile Path:{args.path}"
+        )
+    if not args.path.endswith(".txt"):
+        raise ValueError("Cannot operate on a non `.txt` file!")
 
     return args
 
@@ -57,61 +79,74 @@ LETTERS = [chr(ascii_val) for ascii_val in range(97, 123)]
 
 
 def calc_letter(letter: str, letters: list, offset: int):
-    if letter not in letters: # Check if letter is not valid.
+    if letter not in letters:  # Check if letter is not valid.
         raise ValueError(f"Letter {letter} not in letters to use!")
-    elif len(letter) != 1: # Check if string size is not 1.
+    elif len(letter) != 1:  # Check if string size is not 1.
         raise ValueError("Variable `letter` is a string with size greater than 1!")
     else:
-        ascii_val = ord(letter) + offset # Calculate ciphered value.
+        ascii_val = ord(letter) + offset  # Calculate ciphered value.
         # If value is bigger than top ascii value or lower than bottom ascii value, recalculate.
         while any((ascii_val > ord("z"), ascii_val < ord("a"))):
             if ascii_val > ord("z"):
                 ascii_val -= 26
             elif ascii_val < ord("a"):
                 ascii_val += 26
-        
-        return chr(ascii_val) # Return the calculated character.
+
+        return chr(ascii_val)  # Return the calculated character.
 
 
 def decrypt(text: str, offset: int):
-    if offset > 0: # Check if offset is valid.
+    if offset > 0:  # Check if offset is valid.
         raise ValueError("Decipher offset cannot be positive!")
     output = ""
-    for character in text: # Modify each character accordingly.
-        if character not in LETTERS: # If character is not in the valid letters list then ignore.
+    for character in text:  # Modify each character accordingly.
+        if (
+            character not in LETTERS
+        ):  # If character is not in the valid letters list then ignore.
             output = output + character
             continue
         output = output + calc_letter(character, LETTERS, offset)
-    return output # Return decrypted output.
+    return output  # Return decrypted output.
 
 
 def encrypt(text: str, offset: int):
-    if offset < 0: # Check if offset is valid.
+    if offset < 0:  # Check if offset is valid.
         raise ValueError("Encryption offset cannot be negative!")
     output = ""
-    for character in text: # Modify each character accordingly.
-        if character not in LETTERS: # If character is not in the valid letters list then ignore.
+    for character in text:  # Modify each character accordingly.
+        if (
+            character not in LETTERS
+        ):  # If character is not in the valid letters list then ignore.
             output = output + character
             continue
         output = output + calc_letter(character, LETTERS, offset)
-    return output # Return encrypted output.
+    return output  # Return encrypted output.
 
 
 def main():
-    args = read_arguments() # Read arguments
+    args = read_arguments()  # Read arguments
     offset = args.offset
-    if args.decrypt: # Perform selected task.
+    text = str()
+
+    # Select desired text to operate on.
+    if args.path is not None:
+        with open(args.path, "r") as f:
+            text = f.read()
+    else:
+        text = args.string
+
+    if args.decrypt:  # Perform selected task.
         if offset > 0:
             offset = -offset
         try:
-            print(decrypt(args.string.lower(), offset))
+            print(decrypt(text.lower(), offset))
         except Exception as e:
             print(f"ERROR: {e}")
         finally:
             exit()
     elif args.encrypt:
         try:
-            print(encrypt(args.string.lower(), offset))
+            print(encrypt(text.lower(), offset))
         except Exception as e:
             print(f"ERROR: {e}")
         finally:
